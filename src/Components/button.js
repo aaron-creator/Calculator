9b2f3fe00cb4a53316be8bab14dc20e2edabf1f6
aaron_tetracube <aaronpsam@gmail.com>
2021-10-22 22:16:15 +0530

    added

---
+++ src/Components/button.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "@mui/material/Button";
+import Results from "./outputScreen";
+
+const button = () => {
+  return (
+    <div>
+      <h1>Calculate : </h1>
+      <br></br>
+      <div>Results</div>
+    </div>
+  );
+};
+export default button;
